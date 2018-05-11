3b6759915b2254db7d62e6bdc6d39edc0a16b2f5
quininer <quininer@live.com>
2018-05-11 19:51:58 +0800

    feat: use url hash

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const { foo } = wasm_bindgen;
 
 function run() {
+	const DEFAULT = `{ "type": 0, "message": "YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXo=", "start": 0 }`;
+
 	let input = document.getElementById("input");
 	let submit = document.getElementById("submit");
+	let hash = location.hash;
+
+	if (hash.length == 0) {
+		input.value = DEFAULT;
+	} else {
+		input.value = atob(hash.substr(1));
+	}
 
 	submit.addEventListener("click", () => {
+		location.hash = btoa(input.value);
+
 		for (let line of input.value.split('\n')) {
 			if (line.startsWith("//")) {
 				continue
